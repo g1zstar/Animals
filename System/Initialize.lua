daeb774d80b4431e7250e9c9fba2cbc2e4e573ab
g1zstar <sensiblemagic@gmail.com>
2016-11-16 23:32:28 -0500

    abc

--- System/Initialize.lua
+++
@@ -1,7 +0,0 @@
-local animalsName, animalsTable = ...
-local _ = nil
-
-animalsTable.createMainFrame()
-animalsTable.createMonitorFrame()
-animalsTable.createSlayingInformationFrame()
-animalsTable.createSlayingFrame()
